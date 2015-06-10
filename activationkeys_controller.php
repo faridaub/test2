6b040fb389d595267495cda58347360c78910c39
faridaub <faridjapan112@gmail.com>
2015-06-11 00:44:43 +0900

    updateds

--- activationkeys_controller.php
+++ activationkeys_controller.php
@@ -14,5 +14,9 @@ class ActivationkeysController extends AppController {
 	var $layout = "cake_layout";
     var $scaffold;
 	
+			var $name = 'Activationkeys';
+	var $layout = "cake_layout";
+    var $scaffold;
+	
 }
 ?>
