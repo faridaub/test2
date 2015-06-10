3e7563020dbad68d77cd7d14f2c9c4c8ea66dce2
faridaub <faridjapan112@gmail.com>
2015-06-11 00:11:07 +0900

    i added new html file

---
+++ activationkeys_controller.php
@@ -0,0 +1,18 @@
+<?php
+class ActivationkeysController extends AppController {
+	var $name = 'Activationkeys';
+	var $layout = "cake_layout";
+    var $scaffold;
+	
+	
+	var $name = 'Activationkeys';
+	var $layout = "cake_layout";
+    var $scaffold;
+	
+	
+		var $name = 'Activationkeys';
+	var $layout = "cake_layout";
+    var $scaffold;
+	
+}
+?>
